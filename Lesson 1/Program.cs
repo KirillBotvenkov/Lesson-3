71d16d770b9d02042bde0093d2d52e1f663d92e0
Kirill <botvenkov2016@yandex.ru>
2022-12-30 22:01:21 +0300

    Задача 1

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int ReverseNumber(int number){
+    int reverse = 0;
+    while (number > 0){
+        reverse = reverse * 10 + number % 10;
+        number /= 10;
+    }
+    return reverse;
+}
+
+bool IsPalindrome(int number){
+    int reverse = ReverseNumber(number);
+    return reverse == number;
+}
+bool ValidateNumber(int number){
+    if (number < 10000 || number >= 100000){
+        Console.WriteLine("Число не является пятизначным");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите пятизначное число");
+if (ValidateNumber(number))
+{
+    if (IsPalindrome(number)){
+        Console.WriteLine("Число является палиндромом");
+    }
+    else {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
