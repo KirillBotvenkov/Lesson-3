36ea74bc0c84c3b5965508bb72d964a57b1a0e32
Kirill <botvenkov2016@yandex.ru>
2022-12-30 22:37:35 +0300

    Задача 3

---
+++ Lesson 3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int Promt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int Cube(int number)
+{
+    return number*number*number;
+}
+
+void ShowCube(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        System.Console.WriteLine(Cube(i));
+    }
+}
+
+int number = Promt("Введите число");
+ShowCube(number);
